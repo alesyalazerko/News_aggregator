3b4a27f765019aa0f457b2de279ec6f7b58c7736
demidraksin <greatraksin@icloud.com>
2021-04-18 20:13:41 +0300

    ⭐Add some changes🐋💨

--- app.py
+++ app.py
@@ -1,33 +1,21 @@
 from flask import Flask
 from flask import render_template
 from newsapi import NewsApiClient
-
-
-'''
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from config import Config
-'''
 
 app = Flask(__name__)
-'''
-app.config.from_object(Config)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 
-
-
-db.create_all()
-'''
-
-
 @app.route('/', methods=['GET', 'POST'])
 def Index():
-
     newsapi = NewsApiClient(api_key="87a33682f0fc4efa9f36e2336b721304")
     topheadlines = newsapi.get_top_headlines(sources="cnn")
-
     articles = topheadlines['articles']
 
     desc = []
@@ -42,7 +30,7 @@ def Index():
         img.append(new['urlToImage'])
         url.append(new['url'])
     mylist = zip(news, desc, img, url)
-    return render_template('index.html', context = mylist)
+    return render_template('index.html', context=mylist)
 
 
 if __name__ == "__main__":
