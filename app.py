d576dc0ae8cbc35c522225c5a4668d94012cabac
demidraksin <greatraksin@icloud.com>
2021-04-18 19:15:19 +0300

    ⭐Add some changes🐋💨

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
 from newsapi import NewsApiClient
+
+
 '''
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -25,7 +28,6 @@ def Index():
     newsapi = NewsApiClient(api_key="87a33682f0fc4efa9f36e2336b721304")
     topheadlines = newsapi.get_top_headlines(sources="cnn")
 
-
     articles = topheadlines['articles']
 
     desc = []
@@ -33,21 +35,13 @@ def Index():
     img = []
     url = []
 
-
-    for i in range(len(articles)):
-        myarticles = articles[i]
-
-
-        news.append(myarticles['title'])
-        desc.append(myarticles['description'])
-        img.append(myarticles['urlToImage'])
-        url.append(myarticles['url'])
-
-
-
+    for new in articles:
+        print(new)
+        news.append(new['title'])
+        desc.append(new['description'])
+        img.append(new['urlToImage'])
+        url.append(new['url'])
     mylist = zip(news, desc, img, url)
-
-
     return render_template('index.html', context = mylist)
 
 
